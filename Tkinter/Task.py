5f899312f4e382a9cd652a99d789cd257a032a95
firetween <92782900+firetween@users.noreply.github.com>
2021-12-07 08:54:01 +0200

    Update Task.py

--- Tkinter/Task.py
+++ Tkinter/Task.py
@@ -12,6 +12,33 @@ c=0
 
 answer=[]
 
+def lahenda():    
+    if (a.get()!=""and b.get()!="" and c.get()!=""):
+        a_=int(a.get())
+        b_=int(b.get())
+        c_=int(c.get())
+        D=b_*b_-4*a_*c_
+        if D>0:
+            x1_=round((-1*b_+sqrt(D))/(2*a_),2)
+            x2_=round((-1*b_-sqrt(D))/(2*a_),2)
+            t=f"X1={x1_}, \nX2={x2_}"
+        elif D==0:
+            x1_=round((-1*b_)/(2*a_),2)
+            t=f"X1={x1_}"
+        else:
+            t="Корней нет"
+        vastus.configure(text=f"D={D}\n{t}")
+        a.configure(bg="lightblue")
+        b.configure(bg="lightblue")
+        c.configure(bg="lightblue")
+    else:
+        if a.get()=="":
+            a.configure(bg="red")
+        elif b.get()=="":
+            b.configure(bg="red")
+        elif c.get()=="":
+            c.configure(bg="red")
+
 def text_to_lbl(event):#ent_number
     text=ent1.get()
     a=text
