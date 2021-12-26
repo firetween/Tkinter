7b9c032e1f35187a178a5094e2e19aec9a951015
VadimDmitriev <workvadim@gmail.com>
2021-12-26 21:44:07 +0200

    ui

--- Tkinter/UI.py
+++ Tkinter/UI.py
@@ -20,65 +20,48 @@ def valik():
     val=str(var.get())+", " #value 1,2 või 3
     ent.insert(END,val)
 
-def uus_aken(ind:int):
-    uusaken=Toplevel()
-    tabs=ttk.Notebook(uusaken)
-    texts=["1.png","2.png","3.png","4.png","5.png"]
-    textn=texts
-    tab=[]
-    cann=[]
-    for i in range(len(texts)):
-        tab.append("tab"+str(i))
-        tab[i]=Frame(tabs)
-        tabs.add(tab[i],text=texts[i])#,image=img1)
-        textn[i]=PhotoImage(file=texts[i]).subsample(1)
-        cann.append("cann"+str(i))
-        cann[i]=Canvas(tab[i],height=200,width=300,bg="red")
-        cann[i].create_image(0,0,image=textn[i], anchor=NW)
-        cann[i].pack()
-
-    tabs.grid(row=0,column=0)
-    tabs.select(ind)
-    uusaken.mainloop()
-
 
 
 aken=Tk() #Akna loomine
 aken.title("Window name") #Akna pealkiri
-aken.geometry("600x400") #Akna suurus
-menu=Menu(aken)
-aken.config(menu=menu)
-m1=Menu(menu)
-menu.add_cascade(label="Tabs",menu=m1)
-m1.add_command(label="Tab1",accelerator="Command+A", command=lambda:uus_aken(0))
-m1.add_command(label="Tab2",command=lambda:uus_aken(1))
-m1.add_command(label="Tab3",command=lambda:uus_aken(2))
-m1.add_command(label="Tab4",command=lambda:uus_aken(3))
-m1.add_command(label="Tab5",command=lambda:uus_aken(4))
-m1.add_separator()
+aken.geometry("600x165") #Akna suurus
 
+lbl=Label(aken,text="Решение квадратного уравнения",font="Arial 20",fg="green",bg="lightblue") 
+btn=Button(aken,text="Решить",font="Arial 20",bg="green")
+ent=Entry(aken,width=5,font="Arial 20",fg="green",bg="lightblue")
+lbl2=Label(aken,text=" x**2+ ",width=4,font="Arial 20",fg="green") 
+ent2=Entry(aken,width=4,font="Arial 20",fg="green",bg="lightblue")
+lbl3=Label(aken,text="x+",width=3,font="Arial 20",fg="green")
+ent3=Entry(aken,width=4,font="Arial 20",fg="green",bg="lightblue")
+lbl4=Label(aken,text="=",width=3,font="Arial 20",fg="green")
+lbl5=Label(aken,text="Решение",height=2,width=30,font="Arial 10",bg="yellow")
 
-
-btn=Button(aken,text="Press me",font="Arial 20",fg="green",bg="lightblue", width=20, height=3)#,relief=SUNKEN)#GROOVE, RAISED Loome nupp
-btn2=Button(aken,text="Double window",font="Arial 20",fg="green",bg="lightblue",command=lambda:uus_aken(0))
-lbl=Label(aken,text="...") #Pealkiri
-ent=Entry(aken,fg="blue",width=20,font="Arial 20",justify=CENTER) #Rida teksti sisestamiseks. tekst on keskel
 var=IntVar() #StringVar()
 #var.set(3) valib kolmas button
-r1=Radiobutton(aken,text="Esimene",variable=var,value=1,command=valik)
-r2=Radiobutton(aken,text="Teine",variable=var,value=2,command=valik)
-r3=Radiobutton(aken,text="Kolmas",variable=var,value=3,command=valik)
+#r1=Radiobutton(aken,text="Esimene",variable=var,value=1,command=valik)
+#r2=Radiobutton(aken,text="Teine",variable=var,value=2,command=valik)
+#r3=Radiobutton(aken,text="Kolmas",variable=var,value=3,command=valik)
 
-btn.bind("<Button-1>",klikker)#LKM 
-btn.bind("<Button-3>",klikker2)#PKM
-ent.bind("<Return>",text_to_lbl)#Enter
+#btn.bind("<Button-1>",klikker)#LKM 
+#btn.bind("<Button-3>",klikker2)#PKM
+#ent.bind("<Return>",text_to_lbl)#Enter
+
 lbl.pack()
-btn.pack()
-ent.pack()
-r1.pack(side=LEFT)
-r2.pack(side=LEFT)
-r3.pack(side=LEFT)
-btn2.pack(side=RIGHT)
+ent.pack(side=LEFT)
+lbl2.pack(side=LEFT)
+ent2.pack(side=LEFT)
+lbl3.pack(side=LEFT)
+ent3.pack(side=LEFT)
+lbl4.pack(side=LEFT)
+btn.pack(side=LEFT)
+lbl5.pack()
+lbl5.place(x=180,y=130)
+
+
+#r1.pack(side=LEFT)
+#r2.pack(side=LEFT)
+#r3.pack(side=LEFT)
+
 aken.mainloop()
 
 
